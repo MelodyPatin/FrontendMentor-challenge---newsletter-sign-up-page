ded561004438f8e27b16895fd401130970f6a43d
MelodyPatin <melody.patin@oclock.school>
2023-11-01 13:43:19 +0100

    after feedbacks

--- js/newsletter.js
+++ js/newsletter.js
@@ -2,13 +2,20 @@ const newsletterElement = {
     init: function(){
         const form = document.querySelector("form");
         form.addEventListener("submit", newsletterElement.handleSubmitForm)
+
+        const dismiss = document.querySelector(".dismissButton")
+        dismiss.addEventListener("click", newsletterElement.handleDismissForm)
     },
 
-    isEmailValid: function(email){
-        if(email.includes("@")){ 
-                 return true;
-            }else {return false};
-        },
+    isEmailValid: function(){
+        const input = document.querySelector("#emailForm");
+        const validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (input.value.match(validRegex)) {
+          return true;
+        } else {
+          return false;
+        }
+      },
 
     handleSubmitForm: function(event){
         event.preventDefault();
@@ -35,13 +42,21 @@ const newsletterElement = {
         if(newsletterElement.isEmailValid(input)){
             const container = document.querySelector(".container");
             const popup = document.querySelector(".popup")
-            container.remove();
+            container.style.display = "none";
             popup.style.display = "block"
             const validEmail = document.querySelector("strong");
             validEmail.innerHTML = input;
         }
 
-        const form = document.querySelector("form");
-        form.reset();
+        
+    }, 
+
+    handleDismissForm: function(event){
+        event.preventDefault();
+        const container = document.querySelector(".container");
+        const popup = document.querySelector(".popup")
+        container.style.display = "flex";
+        popup.style.display = "none"
+
     }
 }
